759bb4df17a15414209dfd1806adaf711963c7f8
ANU-2524 <anusoni25.2006@gmail.com>
2026-01-24 19:27:46 +0530

    2.32

---
+++ offline-academy/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // enable class-based dark mode
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: "#93C5FD",
+          DEFAULT: "#3B82F6",
+          dark: "#1E40AF",
+        },
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
+};
